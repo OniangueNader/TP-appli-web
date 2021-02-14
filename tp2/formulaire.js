e90888448abd9f16e176d9175cd242c27e2b1d54
OniangueNader <72713227+OniangueNader@users.noreply.github.com>
2021-02-14 17:59:04 +0100

    Update TP2

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,9 +1,38 @@
 function validation(){
-    var nom = document.getElementById("nom").nodeValue;
-    var errormsg = document.getElementById("error").innerHTML
-    if (nom.length<5){
+    var nom = document.getElementById("nom");
+    var prenom = document.getElementById("prenom");
+    var adresse = document.getElementById("adresse");
+    var mail = document.getElementById("mail");
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var errormsg = document.getElementById("error");
+    var form = document.getElementById('form')
+    if (nom.value.length<5){
         //afficher une erreur.
+        document.getElementById("resultat").innerHTML = '';
+        document.getElementById("error").innerHTML = "le nom est supérieur à 5";
+        form.addEventListener('submit',(e)=>{e.preventDefault()})
+    }
+    else if (prenom.value.length<5){
+        //afficher une erreur.
+        document.getElementById("resultat").innerHTML = '';
+        document.getElementById("error").innerHTML = "le prenom est supérieur à 5";
+        form.addEventListener('submit',(e)=>{e.preventDefault()})
+    }
+    else if (adresse.value.length<5){
+        //afficher une erreur.
+        document.getElementById("resultat").innerHTML = '';
+        document.getElementById("error").innerHTML = "le adresse est supérieur à 5";
+        form.addEventListener('submit',(e)=>{e.preventDefault()})
+    }
+    else if (mail.value.length<5){
+        //afficher une erreur.
+        document.getElementById("resultat").innerHTML = '';
+        document.getElementById("error").innerHTML = "le mail est supérieur à 5";
+        form.addEventListener('submit',(e)=>{e.preventDefault()})
+    }
+    else{
+        document.getElementById("error").innerHTML = '';
+        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
+        
     }
 }
-
-form.addEventListener('submit',(valid)=> {valid.preventDefault})
